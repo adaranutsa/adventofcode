0d7004977b63ebc89bc240f730724fcc81b8e3e2
aleksdaranutsa <aleksdaranutsa@gmail.com>
2021-11-30 21:35:08 -0800

    Adding the template files

---
+++ template/puzzle.py
@@ -0,0 +1,47 @@
+import sys
+
+def puzzle1(data):
+    """
+    Input data should be sanitized.
+
+    The function should return the result.
+    """
+    pass
+
+def puzzle2(data):
+    """
+    Input data should be sanitized.
+
+    The function should return the result.
+    """
+    pass
+
+def is_test():
+    return True if '-t' in sys.argv else False
+
+if __name__ == '__main__':
+    test = is_test()
+
+    test_input = 'puzzle.test.txt'
+    real_input = 'puzzle.input.txt'
+
+    if not test:
+        data_input = real_input
+        msg = "Puzzle"
+    else:
+        data_input = test_input
+        msg = "Test Puzzle"
+
+    with open(data_input, 'r') as f:
+        data = f.readlines()
+
+        # TODO: Sanitize data into proper format. This could be int or simply removing the newlines.
+        #data = [int(x.strip()) for x in data]
+
+    if '-p1' in sys.argv:
+        p1_out = puzzle1(data)
+        print(f"{msg} 1 Output: {p1_out}")
+
+    if '-p2' in sys.argv:
+        p2_out = puzzle2(data)
+        print(f"{msg} 2 Output: {p2_out}")
